1b60c5124aa8068ea4ef1b6926da6ee16e0ae6c4
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-08-02 18:56:04 +0500

    Create variables.tf

---
+++ modules/management/azure_resource_group/variables.tf
@@ -0,0 +1,10 @@
+//------------------------------------General Properties------------------------------------\\
+variable "rgname" {
+  type    = string
+  default = ""
+}
+
+variable "location" {
+  type    = string
+  default = ""
+}
